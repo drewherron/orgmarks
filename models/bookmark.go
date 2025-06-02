2b5329d7eeb5199e13477833002498e9b0387466
Drew Herron <herron@mailbox.org>
2025-06-02 00:43:50 -0700

    Start bookmark and folder structs

--- models/bookmark.go
+++ models/bookmark.go
@@ -1,3 +1,22 @@
 package models
 
-// Data models for bookmarks and folders
+import "time"
+
+// Bookmark represents a single bookmark entry
+type Bookmark struct {
+	URL          string    // The bookmark URL
+	Title        string    // The bookmark title/name
+	Tags         []string  // Tags associated with the bookmark
+	ShortcutURL  string    // Firefox SHORTCUTURL attribute (optional)
+	AddDate      time.Time // When the bookmark was added
+	LastModified time.Time // When the bookmark was last modified
+	Description  string    // Optional description text (below the link in org-mode)
+}
+
+// Folder represents a bookmark folder/directory
+type Folder struct {
+	Title        string      // The folder name
+	Children     []Node      // Child nodes (can be bookmarks or folders)
+	AddDate      time.Time   // When the folder was created
+	LastModified time.Time   // When the folder was last modified
+}
