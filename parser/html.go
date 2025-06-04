fa0fd3659d5db843ec459f319bd030fe85f88ffa
Drew Herron <herron@mailbox.org>
2025-06-03 23:02:40 -0700

    Implement HTML tokenizer wrapper

--- parser/html.go
+++ parser/html.go
@@ -1,3 +1,34 @@
 package parser
 
-// HTML parser for Netscape Bookmark Format
+import (
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+// HTMLParser wraps the golang.org/x/net/html tokenizer
+type HTMLParser struct {
+	tokenizer *html.Tokenizer
+}
+
+// NewHTMLParser creates a new HTML parser from a reader
+func NewHTMLParser(r io.Reader) *HTMLParser {
+	return &HTMLParser{
+		tokenizer: html.NewTokenizer(r),
+	}
+}
+
+// Next advances to the next token and returns the token type
+func (p *HTMLParser) Next() html.TokenType {
+	return p.tokenizer.Next()
+}
+
+// Token returns the current token
+func (p *HTMLParser) Token() html.Token {
+	return p.tokenizer.Token()
+}
+
+// Err returns any error encountered during parsing
+func (p *HTMLParser) Err() error {
+	return p.tokenizer.Err()
+}
