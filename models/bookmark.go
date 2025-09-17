5b4ad18e2b80baecd215337347f2a4407534299a
Drew Herron <herron@mailbox.org>
2025-09-17 10:07:12 -0700

    Define interface for tree stucture

--- models/bookmark.go
+++ models/bookmark.go
@@ -2,6 +2,13 @@ package models
 
 import "time"
 
+// Node is the interface that both Bookmark and Folder implement
+// This allows building a tree structure with mixed node types
+type Node interface {
+	IsFolder() bool
+	GetTitle() string
+}
+
 // Bookmark represents a single bookmark entry
 type Bookmark struct {
 	URL          string    // The bookmark URL
@@ -19,4 +26,24 @@ type Folder struct {
 	Children     []Node      // Child nodes (can be bookmarks or folders)
 	AddDate      time.Time   // When the folder was created
 	LastModified time.Time   // When the folder was last modified
+}
+
+// IsFolder returns false for Bookmark nodes
+func (b *Bookmark) IsFolder() bool {
+	return false
+}
+
+// GetTitle returns the bookmark title
+func (b *Bookmark) GetTitle() string {
+	return b.Title
+}
+
+// IsFolder returns true for Folder nodes
+func (f *Folder) IsFolder() bool {
+	return true
+}
+
+// GetTitle returns the folder title
+func (f *Folder) GetTitle() string {
+	return f.Title
 }
